82dd3d8393aec79c2d92d6c3ed8c11d99eb2504b
somnath yadav <ysomnath22@gmail.com>
2026-07-02 12:03:38 +0530

    Time: 77 ms (25.28%), Space: 106.9 MB (54.94%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,30 +1,19 @@
-import java.util.Stack;
-
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
-
-        int n = temperatures.length;
-        int[] ans = new int[n];
         Stack<Integer> stack = new Stack<>();
 
-        // Traverse from right to left
-        for (int i = n - 1; i >= 0; i--) {
+        int [] ans = new int[temperatures.length];
 
-            // Remove all temperatures that are not warmer
-            while (!stack.isEmpty() &&
-                   temperatures[stack.peek()] <= temperatures[i]) {
+        for(int i = temperatures.length-1; i>=0; i--){
+
+            while(!stack.isEmpty() && temperatures[stack.peek()] <= temperatures[i]){
                 stack.pop();
             }
-
-            // If stack is not empty, top is the next warmer day
-            if (!stack.isEmpty()) {
-                ans[i] = stack.peek() - i;
+            if(!stack.isEmpty()){
+                ans[i] = stack.peek() -i;
             }
-
-            // Push current index
             stack.push(i);
         }
-
         return ans;
     }
 }
