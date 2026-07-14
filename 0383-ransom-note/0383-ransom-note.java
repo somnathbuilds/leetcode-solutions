4d174b777fd8c58114b006c19ba3028ce00147b9
somnath yadav <ysomnath22@gmail.com>
2026-07-14 06:17:39 +0530

    Time: 15 ms (32.35%), Space: 47.1 MB (27.83%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -6,13 +6,12 @@ class Solution {
             char ch = magazine.charAt(i);
             map.put(ch, map.getOrDefault(ch, 0)+1);
         }
-        
         for(int i = 0; i<ransomNote.length(); i++){
             char ch = ransomNote.charAt(i);
             if(!map.containsKey(ch) || map.get(ch) == 0){
                 return false;
             }
-            map.put(ch, map.getOrDefault(ch, 0)-1);  
+            map.put(ch, map.getOrDefault(ch, 0)-1);
         }
         return true;
     }
