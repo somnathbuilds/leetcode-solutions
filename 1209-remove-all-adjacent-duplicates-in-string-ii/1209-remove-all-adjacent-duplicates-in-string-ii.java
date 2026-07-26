3014ff0f2817a6465f7e7f5be0d594c1495ea94a
somnath yadav <ysomnath22@gmail.com>
2026-07-26 22:36:19 +0530

    Time: 28 ms (62.28%), Space: 47.2 MB (58.79%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
@@ -1,33 +1,30 @@
 class Solution {
-
-    class pair {
+    class pair{
         int count;
         char ch;
-
-        pair(char ch, int count) {
-            this.ch = ch;
+        pair(char ch, int count){
             this.count = count;
+            this.ch = ch; 
         }
-    }  
-
-        public String removeDuplicates(String s, int k) {
-            Stack<pair> stack = new Stack<>();
-            for(char ch : s.toCharArray()) {
-                if(stack.isEmpty() || stack.peek().ch != ch){
-                    stack.push(new pair(ch ,1));
-                } else{
-                    stack.peek().count++;
-                    if(stack.peek().count == k){
-                        stack.pop();
-                    }
+    }
+    public String removeDuplicates(String s, int k) {
+        Stack <pair> st = new Stack<>(); 
+        for( char ch : s.toCharArray()) {
+            if(st.isEmpty() || st.peek().ch != ch){
+                st.push(new pair(ch,1));
+            } else{
+                st.peek().count++;
+                if(st.peek().count == k){
+                    st.pop();
                 }
             }
-            StringBuilder sb = new StringBuilder();
-            for(pair p : stack){
-                for(int i = 0; i<p.count; i++){
-                    sb.append(p.ch);
-                }
+        }
+        StringBuilder sb = new StringBuilder();
+        for(pair p : st){
+            for(int i = 0; i<p.count; i++){
+                sb.append(p.ch);
             }
+        }
         return sb.toString();
     }
 }
