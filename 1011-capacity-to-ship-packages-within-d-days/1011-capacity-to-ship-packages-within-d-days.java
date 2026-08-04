dd2a8dc458fb0216f4774ca1caf87d6dfde57284
somnath yadav <ysomnath22@gmail.com>
2026-08-04 08:04:11 +0530

    Time: 11 ms (67.11%), Space: 50.2 MB (38.43%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int shipWithinDays(int[] weights, int days) {
+
+        if(days > weights.length){
+            return -1;
+        }
+        int low = 0;
+        int high = 0;
+        for(int i = 0; i < weights.length; i++){
+            low = Math.max(low, weights[i]);
+            high += weights[i];
+        }
+        while(low <= high){
+            int mid = low + (high -low)/2;
+
+            if(shipCapacity(weights, days, mid)){
+                high = mid -1;
+            } else {
+                low = mid +1;
+            }
+        }
+        return low;
+    }
+    //Helping function to check conveyor transmit given days 
+    public boolean shipCapacity(int[] weights, int days, int mid){
+        int k = 1;
+        int packages = 0;
+        for(int i = 0; i < weights.length; i++){
+            if(packages + weights[i] <= mid){
+                packages += weights[i];
+                continue;
+            } else {
+                k++;
+                packages = weights[i];
+                if(k > days){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
