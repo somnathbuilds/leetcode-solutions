e95640c12af8d0088854cf3a4ed08049af92dc71
somnath yadav <ysomnath22@gmail.com>
2026-08-24 15:31:54 +0530

    Time: 2 ms (99.96%), Space: 49.1 MB (24.33%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
@@ -1,8 +1,8 @@
 class Solution {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
-        //Handle invalid string
+        // handle invalid 
         if(k <= 1){return 0;}
-        
+
         int i = 0;
         int product = 1;
         int count = 0;
