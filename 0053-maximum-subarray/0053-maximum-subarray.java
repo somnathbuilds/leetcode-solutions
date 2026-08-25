c8d0062ff4b34fc6ca3034395b424ac7e7a7b719
somnath yadav <ysomnath22@gmail.com>
2026-08-26 01:15:12 +0530

    Time: 2 ms (4.03%), Space: 77.4 MB (32.43%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -4,9 +4,7 @@ class Solution {
         int result = nums[0];
 
         for(int i = 1; i<nums.length; i++){
-            int opt1 = nums[i];
-            int opt2 = bestsum + nums[i];
-            bestsum = Math.max(opt1, opt2);
+            bestsum = Math.max(nums[i], bestsum + nums[i]);
             result = Math.max(result, bestsum);
         }
         return result;
