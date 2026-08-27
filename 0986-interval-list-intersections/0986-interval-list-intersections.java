88dec56b3f659815a2572e08e3dc15123a0d81aa
somnath yadav <ysomnath22@gmail.com>
2026-08-27 22:16:46 +0530

    Time: 3 ms (99.29%), Space: 47.1 MB (96.06%) - LeetHub

--- 0986-interval-list-intersections/0986-interval-list-intersections.java
+++ 0986-interval-list-intersections/0986-interval-list-intersections.java
@@ -1,33 +1,34 @@
 class Solution {
     public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
-        List<int[]> list = new ArrayList<>();
-        int i = 0;
-        int j = 0;
-        while(i<firstList.length && j<secondList.length){
-            int start1 = firstList[i][0];
-            int end1 = firstList[i][1];
-            int start2 = secondList[j][0];
-            int end2 = secondList[j][1];
+     List<int[]> list = new ArrayList<>();
+     int i = 0;
+     int j = 0;
 
-            if(start1 <= start2){
-                if(end1 >= start2){
-                    int a = Math.max(start1, start2);
-                    int b = Math.min(end1, end2);
-                    list.add(new int[]{a, b});
-                }
-            } else{
-                if(end2 >= start1){
-                    int a = Math.max(start1, start2);
-                    int b = Math.min(end1, end2);
-                    list.add(new int[]{a, b});
-                }
+     while(i < firstList.length && j < secondList.length){
+        int start1 = firstList[i][0];
+        int end1 = firstList[i][1];
+        int start2 = secondList[j][0];
+        int end2 = secondList[j][1];
+
+        if(start2 >= start1){
+            if(end1 >= start2){
+                int a = Math.max(start1, start2);
+                int b = Math.min(end1, end2);
+                list.add(new int[]{a, b});
             }
-            if(end1 < end2){
-                i++;
-            } else{
-                j++;
+        } else{
+            if(end2 >= start1){
+                int a = Math.max(start1, start2);
+                int b = Math.min(end1, end2);
+                list.add(new int[]{a, b});
             }
         }
-        return list.toArray(new int[list.size()][]);
+        if(end1 < end2){
+            i++;
+        } else{
+            j++;
+        }
+      }
+      return list.toArray(new int[list.size()][]);   
     }
 }
