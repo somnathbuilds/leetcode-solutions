1429c9984251d03ceb0e0cd05844ddbec8dc3d10
somnath yadav <ysomnath22@gmail.com>
2026-08-29 02:05:31 +0530

    Time: 0 ms (100%), Space: 80.3 MB (58.62%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -1,20 +1,17 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
         int low = 0;
-        int high = arr.length - 1;
+        int high = arr.length -1;
 
-        while (low < high) {
-            int mid = low + (high - low) / 2;
+        while(low < high){
+            int mid = low + (high - low)/2;
 
-            if (arr[mid] < arr[mid + 1]) {
-                // We are on the increasing slope
-                low = mid + 1;
-            } else {
-                // We are on the decreasing slope or at the peak
+            if(arr[mid] > arr[mid +1]){
                 high = mid;
+            }else{
+                low = mid +1;
             }
         }
-
-        return low; // or return high;
+        return high;
     }
 }
