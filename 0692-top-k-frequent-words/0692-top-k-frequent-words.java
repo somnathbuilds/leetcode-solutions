2a33be853e6b409ac827ec8008f67fc32bf04476
somnath yadav <ysomnath22@gmail.com>
2026-08-31 21:54:27 +0530

    Time: 6 ms (99.66%), Space: 46.9 MB (7.02%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.java
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -1,33 +1,30 @@
 class Solution {
     public List<String> topKFrequent(String[] words, int k) {
 
-        HashMap<String, Integer> map = new HashMap<>();
-        for(String word : words){
-            map.put(word, map.getOrDefault(word, 0) +1);
-        } 
-
-        PriorityQueue <Map.Entry<String, Integer>> pq = new PriorityQueue<>(
-            (a,b) -> {
-                if(!a.getValue().equals(b.getValue())){
-                    return a.getValue() - b.getValue();
-                } else{
-                    return b.getKey().compareTo(a.getKey());
-                }
+     HashMap<String, Integer> map = new HashMap<>();
+     for(String word : words){
+        map.put(word, map.getOrDefault(word, 0) +1);
+     }
+     PriorityQueue<Map.Entry<String, Integer>> pq = new PriorityQueue<>(
+        (a,b) -> {
+            if(!a.getValue().equals(b.getValue())){
+                return a.getValue() - b.getValue();
+            } else{
+                return b.getKey().compareTo(a.getKey());
             }
-        );
-
+        });
         for(Map.Entry<String, Integer> entry : map.entrySet()){
-
-            pq.offer(entry);
+            pq.add(entry);
 
             if(pq.size() > k){
                 pq.poll();
             }
         }
-        LinkedList<String> list = new LinkedList<>();
+        List<String> list = new ArrayList<>();
         while(!pq.isEmpty()){
-            list.addFirst(pq.poll().getKey());
+            list.add(pq.poll().getKey());
         }
+        Collections.reverse(list);
         return list;
     }
 }
