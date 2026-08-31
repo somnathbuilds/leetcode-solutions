d8b4de9d93316a166992b4b97e9d8bd77be329e4
somnath yadav <ysomnath22@gmail.com>
2026-08-31 20:19:52 +0530

    Time: 14 ms (70.77%), Space: 47.6 MB (49.88%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,29 +1,24 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
 
-        //Step:1 Fill the pair of a[0]= element, a[1]=frequency
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int num : nums){
-            map.put(num, map.getOrDefault(num, 0) +1);
+        HashMap<Integer , Integer> map = new HashMap<>();
+        for(int i = 0; i<nums.length; i++){
+            map.put(nums[i], map.getOrDefault(nums[i], 0) +1);
         }
-        //Step:2 make min heap according to a[1]
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)-> a[1] -b[1]);
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1] -b[1]);
 
-        //Step:3 Traverse the HashMap
         for(Map.Entry<Integer, Integer> entry : map.entrySet()){
-        //Step:4 Head me pair add krna
-            pq.offer(new int[]{entry.getKey(),entry.getValue()});
+            pq.add(new int[]{entry.getKey(), entry.getValue()});
 
             if(pq.size() > k){
                 pq.poll();
             }
         }
-        //Step:5 ans stor kro a[0]= element
-        int[] ans = new int[k];
+        int[] arr = new int[k];
 
-        for(int i = k-1; i >= 0; i--){
-            ans[i] = pq.poll()[0];
+        for(int i = k-1; i>=0; i--){
+            arr[i] = pq.poll()[0]; 
         }
-        return ans;
+        return arr;
     }
 }
